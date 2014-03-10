58d53f42529aa146ee1bba424c996890581b644c
Lifan Wu <winmad.wlf@gmail.com>
2014-03-10 23:42:39 +0800

    my first commit

---
+++ machine/MalformedPacketException.java
@@ -0,0 +1,16 @@
+//u`ofe`{i`nlhofgdhbi`nsthvtmhg`o
+//PART OF THE NACHOS. DON'T CHANGE CODE OF THIS LINE
+// PART OF THE MACHINE SIMULATION. DO NOT CHANGE.
+
+package nachos.machine;
+
+/**
+ * Thrown when a malformed packet is processed.
+ */
+public class MalformedPacketException extends Exception {
+    /**
+     * Allocate a new <tt>MalformedPacketException</tt>.
+     */
+    public MalformedPacketException() {
+    }
+}
