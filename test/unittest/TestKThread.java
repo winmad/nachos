2afc4cfa19437c7fd68e070fd0979a48ac2d0a57
td11 <qltedxtc@sina.com>
2014-03-25 08:35:53 +0800

    Finish the testing part

--- test/unittest/TestKThread.java
+++ test/unittest/TestKThread.java
@@ -5,30 +5,53 @@ import nachos.threads.*;
 
 public class TestKThread {
 	public void testJoin() {
-		KThread printA = new KThread(new PrintChar(str , 'A')).setName("forked thread");
+		StringBuffer str = new StringBuffer("");
+		printB = new KThread(new PrintChar(str , 'b', KThread.currentThread())).setName("forked thread");
+			
+		KThread printA = new KThread(new PrintChar(str , 'a', printB)).setName("waiting thread1");
+		KThread printC = new KThread(new PrintChar(str , 'c', printB)).setName("waiting thread2");
+
+
 		printA.fork();
+		printB.fork();
+		printC.fork();
+		
 		printA.join();
-		new PrintChar(str , 'B').run();
+		printB.join();
+		printC.join();
+		
 		System.out.println("=== test task 1: join ===");
 		System.out.println(str);
 	}
 	
+	KThread printB;
+	
 	private class PrintChar implements Runnable {
-		PrintChar(StringBuffer str , char ch) {
+		PrintChar(StringBuffer str , char ch, KThread pengding_thread) {
 			this.str = str;
 			this.ch = ch;
+			this.pending_thread = pending_thread;
 		}
 		
 		@Override
 		public void run() {
-			for (int i = 0; i < 5; i++) {
+			if(ch == 'a' || ch == 'c') {
+				//System.out.println(ch);
+				//pending_thread.join();
+				printB.join();
+			}
+			for (int i = 0; i < 10; i++) {
 				str.append(ch);
 				KThread.yield();
+			}
+			if(str.charAt(0) == 'a') {
+				printB.join();
 			}
 		}
 		
 		private StringBuffer str;
 		private char ch;
+		private KThread pending_thread;
 	}
 	
 	private StringBuffer str = new StringBuffer("");
